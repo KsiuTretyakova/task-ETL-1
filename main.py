321e05808902bd79cbc61dcb394f835e1bb5757e
KsiuTretyakova <hrytsenkoks@gmail.com>
2025-11-19 17:01:18 +0200

    describe and clear dataframe

--- main.py
+++ main.py
@@ -1,8 +1,105 @@
 import pandas as pd
+import numpy as np
 
-df = pd.read_csv("https://s3-eu-west-1.amazonaws.com/shanebucket/downloads/uk-500.csv")
+pd.set_option("display.max_columns", 50)
+pd.set_option("display.width", 90)
 
-print(df.head())
-print(df.info())
 
-print(df.address)
+# 1. Імпорт та первинне дослідження
+
+
+url = "https://s3-eu-west-1.amazonaws.com/shanebucket/downloads/uk-500.csv"
+# url = "data/uk-500.csv"
+
+df_origin = pd.read_csv(url)
+
+COLUMNS_TO_DROP = []
+
+print("\n--- head ---")
+print(df_origin.head())
+
+print("\n--- info ---")
+print(df_origin.info())
+
+print("\n--- describe ---")
+print(df_origin.describe())
+
+print("\n--- describe for str ---")
+print(df_origin.describe(include=[object]).T)
+
+print("--- null ---")
+# print(df.isna().sum())
+print(df_origin.isna().sum().sort_values(ascending=False).head(20))
+
+print("--- duplicated ---")
+print(df_origin.duplicated().sum())
+
+
+print("--- List columns ---")
+# list_col = df.columns
+# print(list(list_col))
+for i, col in enumerate(df_origin.columns):
+    print(f"{i:02d}. {col}")
+
+
+    
+# 2. Очищення даних
+df = df_origin.copy()
+
+if COLUMNS_TO_DROP:
+    print("\n--- delete columns in list ---")
+    df = df.drop(columns=[col for col in COLUMNS_TO_DROP if col in df.columns], errors='ignore')
+
+    # columns = []    
+    # for col in COLUMNS_TO_DROP:
+    #     if col in df.columns:
+    #         columns.append(col)
+else:
+    print("\nCOLUMNS_TO_DROP = []")
+    
+def standardize_text(s):
+    if pd.isna(s):
+        return np.nan
+    
+    if not isinstance(s, str):
+        s = str(s)
+        
+    s = s.strip()
+    s = " ".join(s.split())
+    
+    return s
+
+
+possible_email_cols = [c for c in df.columns if "email" in c.lower()]
+possible_web_cols = [c for c in df.columns if ("web" in c.lower() or "website" in c.lower() or "url" in c.lower())]
+possible_phone_cols = [c for c in df.columns if ("phone" in c.lower() or "telephone" in c.lower() or "tel" in c.lower())]
+possible_fax_cols = [c for c in df.columns if "fax" in c.lower()]
+
+# генерація списку
+# [змінна_циклу(з приміненими операціями) for змінна_циклу in де_проходимося]
+# [0, 1, 2, 3]
+# [n for n in range(4)]
+
+print("\nPossible columns:")
+print("Email cols:", possible_email_cols)
+print("Web cols:", possible_web_cols)
+print("Phone cols:", possible_phone_cols)
+print("Fax cols:", possible_fax_cols)
+
+
+# Приміняємо зміни
+
+for col in df.select_dtypes(include=['object']).columns:
+    df[col] = df[col].apply(standardize_text)
+
+# email    
+for col in possible_email_cols:
+    df[col] = df[col].str.lower()
+    
+# web    
+for col in possible_web_cols:
+    df[col] = df[col].str.lower()
+    
+# clean phone/fax
+def clean_phone(x):
+    pass
